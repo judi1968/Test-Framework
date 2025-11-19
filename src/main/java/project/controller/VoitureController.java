08bfb0250b22891d8b5769df8d7a9bbf27cb2ec5
Judi <127020548+judi1968@users.noreply.github.com>
2025-11-19 15:00:42 +0300

    finish 6 ter

--- src/main/java/project/controller/VoitureController.java
+++ src/main/java/project/controller/VoitureController.java
@@ -3,6 +3,7 @@ package controller;
 import jframework.annotation.Controller;
 import jframework.annotation.Url;
 import jframework.qutils.ModelView;
+import jframework.annotation.RequestParam;
 
 @Controller
 public class VoitureController {
@@ -14,6 +15,15 @@ public class VoitureController {
     @Url("/salama")
     public ModelView akory(int id, String type){
         ModelView modelView = new ModelView();
+        modelView.addData("message", ""+id);
+        modelView.addData("subtitle"," type : "+ type); 
+        modelView.setView("pages/voiture.jsp");
+        return modelView ;
+    }
+
+    @Url("/akory")
+    public ModelView akoryaby(int id, @RequestParam("baba") String type){
+        ModelView modelView = new ModelView(); 
         modelView.addData("message", ""+id);
         modelView.addData("subtitle"," type : "+ type); 
         modelView.setView("pages/voiture.jsp");
